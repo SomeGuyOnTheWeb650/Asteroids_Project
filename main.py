2f90a58c068029ec55e8eb8cb27cf3ad75319cca
SomeGuyOnTheWeb650 <pollockjosef@gmail.com>
2026-02-15 21:12:09 -0800

    added circleshape, hitbox added player visual

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-
+import player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -17,9 +17,10 @@ def main():
                 return
             pass
         screen.fill("black") # Screen is object type pygame.display, so display type? .fill is a method, black is an argument
+        Player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        Player.draw(screen)
         pygame.display.flip() # refresh?
-
-
+        
         dt = clock.tick(60) / 1000
         
 if __name__ == "__main__":
