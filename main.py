dbd13d4a269ad95183e1317649ffb5d10fd41840
SomeGuyOnTheWeb650 <pollockjosef@gmail.com>
2026-02-16 00:32:28 -0800

    realized I haven't added a hitbox yet, this update creates groups, as well as adding player to both updatable and drawable as well as adding player movement, player can now move forward and back as well as rotate left and right its tank controls but still progress

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
+
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 import player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets screen variable, using global constants in constants.py
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player.Player.containers = (updatable, drawable)
+    
+    Player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}") # Same as underneath
     print(f"Screen height: {SCREEN_HEIGHT}") # Don't think it's needed now, but leave it for now
     while True: # infinite loop, game == running
         log_state() # runs the logger.py function log_state
+        
         for event in pygame.event.get(): # calls event in pygame.event, likely an updating list of ongoing changes using user input
             if event.type == pygame.QUIT:
                 return
             pass
         screen.fill("black") # Screen is object type pygame.display, so display type? .fill is a method, black is an argument
-        Player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        Player.draw(screen)
+        
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip() # refresh?
         
         dt = clock.tick(60) / 1000
